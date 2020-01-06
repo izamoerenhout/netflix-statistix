9421f683c9ea9d1454eb1c2b2124f6a8700f3c1e
Zareta <z.usmaeva@student.avans.nl>
2020-01-06 14:50:44 +0100

    Aanmaken BackButton AccountsScreen

--- trio-opdracht-p2-rd2/src/gui/AccountsScreenController.java
+++ trio-opdracht-p2-rd2/src/gui/AccountsScreenController.java
@@ -1,11 +1,24 @@
 package gui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class AccountsScreenController {
 
     public Button buttonCreateAccount;
     public Button buttonEditAccount;
     public Button buttonDeleteAccount;
+    public Button buttonBackAccount;
+    public Stage stage;
 
+    public void returnToMainMenu() throws Exception {
+        stage = Main.getPrimaryStage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        stage.setScene(new Scene(root));
+    }
 }
