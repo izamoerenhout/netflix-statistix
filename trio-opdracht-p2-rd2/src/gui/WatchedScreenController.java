cc9df3fc92a743937f1b7105e59bcf574b9eb88f
Zareta <z.usmaeva@student.avans.nl>
2020-01-06 23:32:06 +0100

    Back Button WatchedScreen

--- trio-opdracht-p2-rd2/src/gui/WatchedScreenController.java
+++ trio-opdracht-p2-rd2/src/gui/WatchedScreenController.java
@@ -1,11 +1,31 @@
 package gui;
 
+import appLogic.Account;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class WatchedScreenController {
+
+
+    public Stage stage;
+
 
     public Button buttonCreateWatched;
     public Button buttonEditWatched;
     public Button buttonDeleteWatched;
+    public Button buttonBack;
 
+    private ObservableList<Account> list;
+
+    public void returnToMainMenu() throws Exception {
+        stage = Main.getPrimaryStage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        stage.setScene(new Scene(root));
+    }
 }
