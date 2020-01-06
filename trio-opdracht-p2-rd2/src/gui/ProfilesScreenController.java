be8988f82e7bae4ee3ea3103ef9b63721471165a
Zareta <z.usmaeva@student.avans.nl>
2020-01-06 23:28:30 +0100

    Back Button ProfilesScreen

--- trio-opdracht-p2-rd2/src/gui/ProfilesScreenController.java
+++ trio-opdracht-p2-rd2/src/gui/ProfilesScreenController.java
@@ -1,10 +1,31 @@
 package gui;
 
+import appLogic.Account;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ProfilesScreenController {
+
+
+    public Stage stage;
+
 
     public Button buttonCreateProfile;
     public Button buttonEditProfile;
     public Button buttonDeleteProfile;
+    public Button buttonBack;
+
+    private ObservableList<Account> list;
+
+    public void returnToMainMenu() throws Exception {
+        stage = Main.getPrimaryStage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        stage.setScene(new Scene(root));
+    }
 }
